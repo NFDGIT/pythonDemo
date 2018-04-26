28265e9083c28dcdc94f1f03187b93d26e512671
NFDGIT <942261721@QQ.COM>
2018-04-26 12:21:32 +0800

    添加 工作证明

--- blockchain.py
+++ blockchain.py
@@ -65,11 +65,41 @@ class Blockchain(object):
         return hashlib.sha256(block_string).hexdigest()    
    
 
-
     @property
     def last_block(self):
         return self.chain[-1]
         
 
+
+
+    # 工作量证明
+    def proof_of_work(self, last_proof):
+        """
+        简单的工作量证明:
+         - 查找一个 p' 使得 hash(pp') 以4个0开头
+         - p 是上一个块的证明,  p' 是当前的证明
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        验证证明: 是否hash(last_proof, proof)以4个0开头?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        print(f'{last_proof}+{proof}hash:{guess_hash}')
+        return guess_hash[:4] == "0000"
 
  
