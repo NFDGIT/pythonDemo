8e3b609937e21e5d5239228809dba426a08f1526
NFDGIT <942261721@QQ.COM>
2018-04-26 16:07:50 +0800

    add 区块链运行

--- server.py
+++ server.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
 import blockchain
+import json
+
 from uuid import uuid4
 from flask import Flask
 
 # isinstance our node
 app = Flask(__name__)
 
-# Genesis a globally unique address for this node
-node_indentifier = str(uuid4()).replace('-','')
+indentifier = str(uuid4()).replace("-","")
 
-# isinstance this Blockchain
-blockchain  = blockchain.Blockchain
+blockchain =  blockchain.Blockchain()
 
-@app.route('/mine')
+
+# 挖矿
+@app.route('/mine',methods = ['GET'])
 def mine():
-   return 'helo swift'
-
-
-
+   nextProof = blockchain.proof_of_work(blockchain.last_block["proof"])
    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)    
+   blockchain.new_transaction(sender="0",recipient=indentifier,amount=1)
+
+   blockchain.new_block(nextProof)
+   return json.dumps(blockchain.chain)
+
+
+
+
+
+
+
+# 交易
+@app.route('/transactions/new',methods = ["POST"])
+def new_transaction():
+   pass
+
+
+# 获取整个区块链数据
+@app.route('/chain',methods = ["POST"])
+def chain():
+   return json.dumps(blockchain.chain)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000)
 
